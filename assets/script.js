6c7860764d48fc2127463000963d9fb67f2aa1c7
unknown <tyler.kd.knapp@gmail.com>
2023-12-17 21:35:20 -0600

    Added function to fetch a city's weather

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,26 @@ console.log("This is working!")
 var APIKey = "bf2f5ecc787085a6aa0763fa9d92df94";
 
 var searchButton = $(".btn");
-var inputCityEl = $("#inputCity");
+var inputCityEl = document.getElementById("inputCity");
 var fiveDayForecastEl = $(".fiveDayForecast");
 var weatherIconEl = $(".weather-icon");
-
+var citySearchEl = document.getElementById("citySearch");
 var recentlySearchedCities = [];
 var recentSearchHistoryEl = $("#recentSearchHistory")
+
+function getWeather () {
+    city = inputCityEl.value;
+    fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}`
+    )
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+citySearchEl.addEventListener("click", (event) => {
+    event.preventDefault();
+    getWeather();
+});
