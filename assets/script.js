c5d423935823eba1f1e38ac3c30e1d2990acff00
unknown <tyler.kd.knapp@gmail.com>
2023-12-17 20:15:24 -0600

    Updated HTML and CSS / Added jQuery variables in JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 console.log("This is working!")
 // Variable for API Key that can be reused in code without having to type it repeatedly
 var APIKey = "bf2f5ecc787085a6aa0763fa9d92df94";
+
+var searchButton = $(".btn");
+var inputCityEl = $("#inputCity");
+var fiveDayForecastEl = $(".fiveDayForecast");
+var weatherIconEl = $(".weather-icon");
+
+var recentlySearchedCities = [];
+var recentSearchHistoryEl = $("#recentSearchHistory")
+
+
+
