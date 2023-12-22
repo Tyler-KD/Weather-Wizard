0db9838ecf824d829aed503dd8d92030a624f90d
unknown <tyler.kd.knapp@gmail.com>
2023-12-22 12:56:36 -0600

    Updated README file

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Weather-Wizard
+# Weather Wizard
+
+## Description
+
+Weather Wizard is an application for retrieving weather data for cities.  I built this project to practice and implement Third-Party APIs to access their data and functionality by making requests with specific parameters to a URL.  It solves the problem of wanting to see the weather outlook for multiple cities so that a user can plan a trip accordingly.  Throughout the Weather Wizard project, I learned that JSON format API responses give a plethora of measured or calculated data, a mix of both DOM and jQuery methods can be used to fetch and display icons, recently searched items can be displayed after clicking search without refreshing the page, and the openweathermap API calls more elements accross 5 days than needed because each index of elements is set at the 3 hour mark.  
+
+## Installation
+
+In order to fetch openweathermap API data, a specific API Key must be requested and stored within a JavaScript variable for easy reuse.  When constructing a query URL to make the API call, parameters give variable search terms that are added to an API call to specify requested data.  The API Key variable is concatenated at the end of the URL.  Calling the Fetch API passes the query URL in as a parameter and receives data.  Finally, the response data that's returned by the query can be used for storing and displaying to the webpage.
+
+## Usage
+
+To run Weather Wizard, load the page and type a city within the search box labeled "Search for a City."  Press "Enter" or click "Search" and the "Weather Forecast" will be displayed onscreen.  At the top and just below "Weather Forecast" is the current day weather.  Parameters included are the city name, date, an icon representation of the weather, temperature, wind speed, and humidity.  Just below the current day weather is the 5 day weather forecast for the searched city.  On the left side just below "Search" is the "Recent Cities" list.  Here, every searched city is saved to the local storage and displayed in a list after each click.  With a maximum of 10 Recent Cities displayed, each city has a button appended.  After clicking any "Recent City", the "Weather Forecast" will populate with that city's data.  Finally, the page can be refreshed and the recently searched cities will persist underneath "Recent Cities."  These instructions should help explain the dynamic features for a weather dashboard which retrieves current day and 5 day forecast data.
+
+Attached is a screenshot before searching any cities:
+
+![Before Search Screenshot](./assets/images/WeatherWizardBeforeSearch.png)
+
+Attached is a screenshot after searching a city:
+
+![After Search Screenshot](./assets/images/WeatherWizardAfterSearch.png)
+
+Attached is a screenshot displaying the maximum number of recently searched cities:
+
+![Recently Searched Cities](./assets/images/WeatherWizardRecentlySearched.png)
+
+## Deployed Site
+
+
+
+## Credits
+
+[How to Use API Keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys)
+
+[Current weather data](https://openweathermap.org/current#name)
+
+[Weather Conditions](https://openweathermap.org/weather-conditions)
+
+[5 day weather forecast](https://openweathermap.org/forecast5#name5)
+
+[JavaScript Array Includes() Method](https://www.w3schools.com/jsref/jsref_includes_array.asp)
+
+[Addition assignment (+=) - JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment)
+
+[Moment.js](https://momentjs.com/)
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Tyler-KD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Features
+
+Included features: bootstrap, jQuery, openweathermap API, and Moment.js a Third-Party Library to parse, validate, manipulate, and display dates and times in JS.
--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,40 @@
-console.log("This is working!")
 // Variable for API Key that can be reused in code without having to type it repeatedly
 var apiKey = "bf2f5ecc787085a6aa0763fa9d92df94";
 
-var searchButton = $(".btn");
+// User's input text for searching cities 
 var inputCityEl = document.querySelector("#inputCity");
-var fiveDayForecastEl = $(".fiveDayForecast");
-var fiveDayWeatherIconEl = $("5DayIcon");
+// Variable for Current Day Forecast icon
 var currentWeatherIconEl = $(".currentWeatherIcon");
+// Search button
 var citySearchEl = document.getElementById("citySearch");
+// empty array for recently searched cites
 var recentlySearchedCities = [];
+// List of recently searched cites
 var recentSearchHistoryEl = $("#recentSearchHistory")
 
+// Retrieves recentlySearchedCities value from Key: "City Name"
 var SearchedGet = localStorage.getItem("City Name");
 if (SearchedGet !== null) {
     recentlySearchedCities = JSON.parse(SearchedGet);
     console.log(recentlySearchedCities);
 }
+// Call displays Recent Cities everytime page is loaded
 displaySearchedCities();
 
+// Function for fetching and displaying Current Day forecast data
+// Trimmed leading and trailing spaces from user input to prevent API Request errors
 function getWeather(city) {
     city = (city || inputCityEl.value).trim();
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-    )
+    )   
+        // If there is a response, then city is converted to JSON for usable data
         .then(function (res) {
             return res.json();
         })
         .then(function (data) {
+            // Function for applying text data from API to city, date, weather icon, wind speed, and humidity elements
+            // If there are data, city name, weather icon, temperature, wind speed, and humidity values, then the function runs
             if (data && data.name && data.weather[0].icon && data.main.temp && data.wind.speed && data.main.humidity) {
 
 
@@ -36,9 +44,10 @@ function getWeather(city) {
                 console.log("temp", data.main.temp);
                 console.log("wind speed", data.wind.speed);
                 console.log("humidity", data.main.humidity);
+                // .text method applies stringed data values to id elements
                 $("#city").text(`${data.name}`);
                 $("#date").text(`${moment().format("L")}`);
-
+                // Variable for created image element
                 var currentWeatherIconEl = document.createElement("img");
                 currentWeatherIconEl.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
                 currentWeatherIconEl.setAttribute("alt", `Weather Icon`);
@@ -51,12 +60,16 @@ function getWeather(city) {
                 $("#humidity").text(`Humidity: ${data.main.humidity} %`);
                 $("#wind").text(`Wind: ${data.wind.speed} mph`);
 
+                // Conditional to prevent duplicates from displaying in Recent Cities list
                 if (!recentlySearchedCities.includes(city)) {
                     recentlySearchedCities.push(city);
                 }
                 count = 0;
                 // if array is too long, cut it down if higher than specifized number, splice method
+                // Key: "City Name" Value: receentlysearchCities
+                // Strings the recentlySearchedCities array and saves data to local storage
                 localStorage.setItem("City Name", JSON.stringify(recentlySearchedCities));
+                // Calls displaySearchedCities function so that recently searched cities are displayed after each user clicks "search" 
                 displaySearchedCities();
             } else {
                 console.error("Invalid data received from the API");
@@ -64,14 +77,20 @@ function getWeather(city) {
         })
 };
 
+// Event listener for Search button
+// After user click "Search," Current Forecast and 5 Day Forecast functions are called
 citySearchEl.addEventListener("click", (event) => {
     event.preventDefault();
     getWeather();
     get5Day();
 });
 
+// Function for displaying 5 Day Forecast data
 function generateCard(apiData) {
+    // Index starts at 0
     var index = 0
+    // API returns 40 elements across 5 days  Every 8 elements is for the course of a day.  If you make i increase by 8 each time, then it will be 0, 8, 16, 24, 32
+    // Even though you are only retrieving 5 elements, you are just indexing every 8th because 40/5 = 8.  Each index is set at 3 hour mark.  3 hours * 8 = 24 hours a day
     for (let j = 1; j < apiData.length; j = j + 8) {
 
         $(`#day-${index}-date`).text(`Date: ${moment().add(index + 1, "days").format("L")}`);
@@ -83,11 +102,13 @@ function generateCard(apiData) {
         $(`#day-${index}-temp`).text(`Temp: ${apiData[j].main.temp} ºF`);
         $(`#day-${index}-wind`).text(`Wind: ${apiData[j].wind.speed} mph`);
         $(`#day-${index}-humidity`).text(`Humidity: ${apiData[j].main.humidity} %`);
-
+        // += adds the right hand side to the variable on the left hand side
+        // will increment each index value by 1 for 5 days
         index += 1
     }
 };
 
+// Function for fetching 5 Day Forecast data
 function get5Day(city) {
     city = (city || inputCityEl.value).trim();
     fetch(
@@ -98,28 +119,34 @@ function get5Day(city) {
         })
         .then(function (data) {
             console.log(data)
+            // Calls generateCard function
             generateCard(data.list);
         })
 };
 
-
+// Function for appending buttons to recently searched cities list
 function displaySearchedCities() {
     recentSearchHistoryEl.empty();
     for (var i = 0; i < recentlySearchedCities.length; i++) {
+        // Variable is recently searched cities converted to list item buttons
         var listBtn = $(`
         <li>
         <button type="button" data-city="new-york" class="btnCity";>
         ${recentlySearchedCities[i]}
         </button>
         </li>`);
+        // Appends city list buttons to Recent Cities div
         recentSearchHistoryEl.append(listBtn);
-
+        // If the length of recently searched cities is greater than 10, then splice elements
         if (recentlySearchedCities.length > 10) {
+            // Splice removes elements from an array and inserts new elements in their place
             recentlySearchedCities.splice(i, 1);
         }
     }
 };
 
+// Event listener for list of recently searched cities / buttons
+// Calls the current day forecast and 5 day forecast functions after clicking button
 recentSearchHistoryEl.on("click", "button", function () {
     console.log($(this).text());
     var city = $(this).text();
